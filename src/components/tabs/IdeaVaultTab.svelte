e761f133edfe13585534b3673859c3c606b7c47d
katokato0605-png <haruking.0605@icloud.com>
2026-06-14 02:57:37 +0900

    fix: IdeaVaultTab.svelte のBOM除去

--- src/components/tabs/IdeaVaultTab.svelte
+++ src/components/tabs/IdeaVaultTab.svelte
@@ -1,4 +1,4 @@
-﻿<script lang="ts">
+<script lang="ts">
   import { onMount } from 'svelte'
   import { ideaStore } from '../../lib/stores/ideaStore.svelte'
   import { projectStore } from '../../lib/stores/projectStore.svelte'
